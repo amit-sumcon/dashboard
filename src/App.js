439d2b38ee59d00cb52d748389c8d299c4a55650
Amit Kandar <kandaramit2001@gmail.com>
2024-04-24 01:29:27 +0530

    Update App.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import EditNavbar from './components/EditNavbar'
-import Edit from './components/Edit'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Edit from './components/Edit';
+import AboutUs from './pages/AboutUs';
+import Dashboard from './pages/Dashboard';
+import Layout from './shared/Layout';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
   return (
-    <div>
-      {/* <EditNavbar/> */}
-      <Edit/>
+    <div className={`${isDarkMode ? 'dark' : 'light'}`}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout setIsDarkMode={setIsDarkMode} />}>
+            <Route index={true} element={<Dashboard />} />
+            <Route path='pages'>
+              <Route path='about-us' element={<AboutUs />} />
+            </Route>
+            <Route path='navbar'>
+              <Route path='edit-navbar' element={<Edit />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+
     </div>
   )
 }
