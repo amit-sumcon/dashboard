b3b1ef04beeea53fa25c0b4491734b1e5713610d
Amit Kandar <kandaramit2001@gmail.com>
2024-04-25 01:29:01 +0530

    Fixed all the bugs and improved

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -2,6 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 
+const pages = [
+    { label: "Hero Section", path: "/pages/hero-section" },
+    { label: "About Us", path: "/pages/about-us" },
+    { label: "Courses", path: "/pages/courses" },
+    { label: "Pricing Page", path: "/pages/pricing-page" },
+    { label: "Why Us", path: "/pages/why-us" },
+    { label: "Client Data", path: "/pages/client-data" },
+    { label: "Why Best Option", path: "/pages/why-best-option" },
+    { label: "Faculty Excellence", path: "/pages/faculty-excellence" },
+    { label: "FAQ", path: "/pages/faq" },
+    { label: "Success Stories", path: "/pages/success-stories" },
+    { label: "Subscribe", path: "/pages/subscribe" },
+]
+
 function SideNav({ isMenu, setIsMenu }) {
     const [isCollapsed, setIsCollapsed] = useState(false);
     const [isLocked, setIsLocked] = useState(true);
@@ -44,14 +58,15 @@ function SideNav({ isMenu, setIsMenu }) {
     const isActive = (path) => {
         return location.pathname === path;
     };
+
     return (
         <div
-            className={`${isCollapsed ? 'w-20 duration-200 transition-all ease-in' : 'w-64 transition-all duration-300 ease-out'} h-screen bg-sky-900 text-white dark:bg-slate-900 dark:text-white ${!isMenu && !isLargeView && 'hidden'} absolute z-50 lg:static lg:z-0`}
+            className={`${isCollapsed ? 'w-24 duration-200 transition-all ease-in' : 'w-64 transition-all duration-300 ease-out'} h-screen bg-sky-900 text-white dark:bg-slate-900 dark:text-white ${!isMenu && !isLargeView && 'hidden'} absolute z-50 lg:static lg:z-0`}
             id='sidebar'
         >
             <div className='flex justify-between items-center gap-16 pt-6 pb-3 px-4 dark:bg-slate-900'>
                 <div className='flex items-center gap-2'>
-                    <span className={`${isCollapsed && 'hidden'} text-xl font-bold`}>Thrive</span>
+                    <span className={`text-xl font-bold`}>Thrive</span>
                 </div>
                 {
                     isLargeView
@@ -71,53 +86,30 @@ function SideNav({ isMenu, setIsMenu }) {
             </div>
             <div className='w-full h-full overflow-y-scroll no-scrollbar'>
                 <div className='flex flex-col gap-4 mb-20 mt-5 pl-6 w-full'>
-                    <LinkItem isActive={isActive('/')} isCollapsed={isCollapsed} label="DashBoard" path="/" icon_name="home-smile" />
+                    <LinkItem isActive={isActive('/')} label="DashBoard" path="/" icon_name="home-smile" />
                     <div className='flex flex-col gap-3 w-full'>
-                        <span className={`${isCollapsed && 'hidden'} text-sm text-gray-500`}>Pages</span>
-                        <LinkItem isActive={isActive('/pages/hero')} isCollapsed={isCollapsed} path="/pages/hero" label="Hero" icon_name="list-ol" />
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/pages/hero" className={`${isCollapsed && 'hidden'}`}>Hero</Link>
+                        <div className='flex flex-col gap-3 w-full'>
+                            <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>Pages</span>
+                            {
+                                pages.map((item, index) => (
+                                    <LinkItem isActive={isActive(item.path)} path={item.path} label={item.label} icon_name="list-ol" key={index} />
+                                ))
+                            }
                         </div>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/pages/about-us" className={`${isCollapsed && 'hidden'}`}>About Us</Link>
+                        <div className='flex flex-col gap-3 w-full'>
+                            <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>Settings</span>
+                            <LinkItem isActive={isActive('/settings/navbar-setting')} label="Navbar Settings" path="/settings/navbar-setting" icon_name="list-ol" />
+                            <LinkItem isActive={isActive('/settings/footer-setting')} label="Footer Settings" path="/settings/footer-setting" icon_name="list-ol" />
                         </div>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/pages/services" className={`${isCollapsed && 'hidden'}`}>Service</Link>
+                        <div className='flex flex-col gap-3 w-full'>
+                            <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>User</span>
+                            <LinkItem isActive={isActive('/user/list')} label="List" path="/user/list" icon_name="list-ol" />
+                            <LinkItem isActive={isActive('/user/view')} label="View" path="/user/view" icon_name="street-view" />
                         </div>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/pages/why-choose-us" className={`${isCollapsed && 'hidden'}`}>Why Choose Us</Link>
-                        </div>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/pages/testimonials" className={`${isCollapsed && 'hidden'}`}>Testimonials</Link>
-                        </div>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/pages/contact-us" className={`${isCollapsed && 'hidden'}`}>Contact Us</Link>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-3 w-full'>
-                        <span className={`${isCollapsed && 'hidden'} text-sm text-gray-500`}>User</span>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-list-ol text-2xl' ></i>
-                            <Link to="/user/list" className={`${isCollapsed && 'hidden'}`}>List</Link>
-                        </div>
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-street-view text-2xl'></i>
-                            <Link to="/user/view" className={`${isCollapsed && 'hidden'}`}>View</Link>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-3 w-full'>
-                        <span className={`${isCollapsed && 'hidden'} text-sm text-gray-500`}>Roles & Permissions</span>
-                        <LinkItem isActive={isActive('/navbar/navbar-settings')} isCollapsed={isCollapsed} label="Navbar Settings" path="/navbar/navbar-settings" icon_name="list-ol" />
-
-                        <div className='flex items-center gap-3'>
-                            <i className='bx bx-lock-alt text-2xl' ></i>
-                            <Link to="/roles-permissions/permissions" className={`${isCollapsed && 'hidden'}`}>Permissions</Link>
+                        <div className='flex flex-col gap-3 w-full'>
+                            <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>Roles & Permissions</span>
+                            <LinkItem isActive={isActive('/roles-permissions/roles')} label="Roles" path="/roles-permissions/roles" icon_name="check-shield" />
+                            <LinkItem isActive={isActive('/roles-permissions/permissions')} label="Permissions" path="/roles-permissions/permissions" icon_name="lock-alt" />
                         </div>
                     </div>
                 </div>
@@ -128,12 +120,12 @@ function SideNav({ isMenu, setIsMenu }) {
 
 export default SideNav
 
-const LinkItem = ({ isCollapsed, path, isActive, label, icon_name }) => {
+const LinkItem = ({ path, isActive, label, icon_name }) => {
     return (
         <div className='relative'>
-            <div className={`flex items-center gap-3 text-white py-2 px-3 rounded-s-xl ${isActive ? "rounded-custom" : ""}`}>
+            <div className={`flex items-center gap-3 cursor-pointer text-white py-2 px-3 rounded-s-xl hover:bg-white hover:text-sky-900 duration-300 ${isActive ? "rounded-custom" : ""}`}>
                 <i className={`bx bx-${icon_name} text-2xl`}></i>
-                <Link to={path} className={`${isCollapsed && 'hidden'}`}>{label}</Link>
+                <Link to={path}>{label}</Link>
             </div>
         </div>
     )
