a5ab5a8389eda3bc1f422f8f09b92bf8accd460d
Amit Kandar <kandaramit2001@gmail.com>
2024-04-26 00:37:12 +0530

    Added Profile dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar({ setIsDarkMode, setIsMenu }) {
-    const [darkMode, setDarkMode] = useState(false);
+function Navbar({ setIsMenu }) {
+    const [isShow, setIsShow] = useState(false);
+    const dropdownRef = useRef(null);
 
     useEffect(() => {
-        setIsDarkMode(darkMode)
-    }, [darkMode, setIsDarkMode])
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsShow(false);
+            }
+        };
 
-    const handleDarkMode = () => {
-        setDarkMode(prev => !prev)
-    }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const handleShow = () => {
+        setIsShow(!isShow);
+    };
 
     return (
-        <nav className='w-full py-4 shadow-md border rounded-md px-5 md:px-10'>
+        <nav className='relative w-full py-3 shadow-md border rounded-md px-5 md:px-10' ref={dropdownRef}>
             <div className='w-full h-full flex justify-between items-center'>
                 <div className='w-fit h-full flex items-center gap-3'>
-                    <div className='dark:text-white lg:hidden' onClick={() => { setIsMenu(true) }}>
+                    <div className='lg:hidden' onClick={() => { setIsMenu(true) }}>
                         <i className='bx bx-menu text-3xl cursor-pointer' ></i>
                     </div>
                 </div>
                 <div className='flex items-center gap-10 md:gap-5'>
-                    <div className='cursor-pointer'>
-                        <i className='bx bx-category dark:text-gray-300'></i>
-                    </div>
-                    <div className='cursor-pointer' onClick={handleDarkMode}>
-                        {
-                            darkMode
-                                ? <i className='bx bx-moon text-gray-300' ></i>
-                                : <i className='bx bx-sun text-xl'></i>
-                        }
-                    </div>
-                    <div className='cursor-pointer'>
-                        <i className='bx bxs-user-circle text-lg dark:text-gray-300'></i>
+                    <div className='cursor-pointer' onClick={handleShow}>
+                        <i className='bx bxs-user-circle text-2xl'></i>
                     </div>
                 </div>
+            </div>
+            <div className={`absolute w-52 h-40 flex flex-col gap-3 right-0 bottom-[-170px] py-4 px-4 rounded-md border shadow-md bg-white ${isShow ? 'block' : 'hidden'}`}>
+                <Link to="/" className='flex items-center gap-3'>
+                    <i className='bx bx-user text-2xl'></i>
+                    <span className='text-base'>Profile</span>
+                </Link>
+                <Link to="/" className='flex items-center gap-3'>
+                    <i className='bx bx-lock-open-alt text-2xl'></i>
+                    <span className='text-base'>Change Password</span>
+                </Link>
+                <Link to="/" className='flex items-center gap-3'>
+                    <i class='bx bx-log-out text-2xl'></i>
+                    <span className='text-base'>Logout</span>
+                </Link>
             </div>
         </nav>
     )
