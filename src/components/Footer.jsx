ad4e7560ff160d156cf48b7234ba422f6a6b7af8
Amit Kandar <kandaramit2001@gmail.com>
2024-04-25 21:53:50 +0530

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer className='w-full flex items-center border border-gray-500 mt-5 p-3 sm:p-5 rounded-md text-gray-600 dark:text-gray-400 mb-5'>
+            <div className='w-full flex items-center justify-between flex-col gap-3 sm:flex-row'>
+                <span className='text-sm'>Designed & Developed By <Link to="https://metawebtx.com/" className='text-sky-900'>@Metawebtx</Link>. All rights reserved.</span>
+                <span className='space-x-5 w-fit flex justify-between'>
+                    <Link to="/terms">Terms</Link>
+                    <Link to="/privacy">Privacy</Link>
+                </span>
+            </div>
+        </footer>
     )
 }
 
