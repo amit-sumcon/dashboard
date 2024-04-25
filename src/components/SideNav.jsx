28b881032d0aa0dfe863e5d30c0541268f81c1b2
Amit Kandar <kandaramit2001@gmail.com>
2024-04-25 21:54:04 +0530

    Fix the bug

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -86,30 +86,30 @@ function SideNav({ isMenu, setIsMenu }) {
             </div>
             <div className='w-full h-full overflow-y-scroll no-scrollbar'>
                 <div className='flex flex-col gap-4 mb-20 mt-5 pl-6 w-full'>
-                    <LinkItem isActive={isActive('/')} label="DashBoard" path="/" icon_name="home-smile" />
+                    <LinkItem isActive={isActive('/')} label="DashBoard" path="/" icon_name="home-smile" isCollapsed={isCollapsed} />
                     <div className='flex flex-col gap-3 w-full'>
                         <div className='flex flex-col gap-3 w-full'>
                             <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>Pages</span>
                             {
                                 pages.map((item, index) => (
-                                    <LinkItem isActive={isActive(item.path)} path={item.path} label={item.label} icon_name="list-ol" key={index} />
+                                    <LinkItem isActive={isActive(item.path)} path={item.path} label={item.label} icon_name="list-ol" key={index} isCollapsed={isCollapsed} />
                                 ))
                             }
                         </div>
                         <div className='flex flex-col gap-3 w-full'>
                             <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>Settings</span>
-                            <LinkItem isActive={isActive('/settings/navbar-setting')} label="Navbar Settings" path="/settings/navbar-setting" icon_name="list-ol" />
-                            <LinkItem isActive={isActive('/settings/footer-setting')} label="Footer Settings" path="/settings/footer-setting" icon_name="list-ol" />
+                            <LinkItem isActive={isActive('/settings/navbar-setting')} label="Navbar Settings" path="/settings/navbar-setting" icon_name="list-ol" isCollapsed={isCollapsed} />
+                            <LinkItem isActive={isActive('/settings/footer-setting')} label="Footer Settings" path="/settings/footer-setting" icon_name="list-ol" isCollapsed={isCollapsed} />
                         </div>
                         <div className='flex flex-col gap-3 w-full'>
                             <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>User</span>
-                            <LinkItem isActive={isActive('/user/list')} label="List" path="/user/list" icon_name="list-ol" />
-                            <LinkItem isActive={isActive('/user/view')} label="View" path="/user/view" icon_name="street-view" />
+                            <LinkItem isActive={isActive('/user/list')} label="List" path="/user/list" icon_name="list-ol" isCollapsed={isCollapsed} />
+                            <LinkItem isActive={isActive('/user/view')} label="View" path="/user/view" icon_name="street-view" isCollapsed={isCollapsed} />
                         </div>
                         <div className='flex flex-col gap-3 w-full'>
                             <span className={`${isCollapsed && 'hidden'} text-sm text-gray-400`}>Roles & Permissions</span>
-                            <LinkItem isActive={isActive('/roles-permissions/roles')} label="Roles" path="/roles-permissions/roles" icon_name="check-shield" />
-                            <LinkItem isActive={isActive('/roles-permissions/permissions')} label="Permissions" path="/roles-permissions/permissions" icon_name="lock-alt" />
+                            <LinkItem isActive={isActive('/roles-permissions/roles')} label="Roles" path="/roles-permissions/roles" icon_name="check-shield" isCollapsed={isCollapsed} />
+                            <LinkItem isActive={isActive('/roles-permissions/permissions')} label="Permissions" path="/roles-permissions/permissions" icon_name="lock-alt" isCollapsed={isCollapsed} />
                         </div>
                     </div>
                 </div>
@@ -120,12 +120,12 @@ function SideNav({ isMenu, setIsMenu }) {
 
 export default SideNav
 
-const LinkItem = ({ path, isActive, label, icon_name }) => {
+const LinkItem = ({ path, isActive, label, icon_name, isCollapsed }) => {
     return (
         <Link to={path} className='relative'>
             <div className={`flex items-center gap-3 cursor-pointer text-white py-2 px-3 rounded-s-xl hover:bg-white hover:text-sky-900 duration-300 ${isActive ? "rounded-custom" : ""}`}>
                 <i className={`bx bx-${icon_name} text-2xl`}></i>
-                <span>{label}</span>
+                <span className={`${isCollapsed && 'hidden'}`}>{label}</span>
             </div>
         </Link>
     )
